88ae662170574c1dd33ac09bc3025da98f4dc8bc
vagrant <Juelz Lum>
2023-03-12 07:55:06 +0000

    add user controller to start email and password with bcrypt

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,10 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+
+    get '/signup' => 'users#new'
+    post '/users' => 'users#create'
+  
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
